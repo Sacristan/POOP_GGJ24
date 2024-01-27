18ad9f69c90fb2a2b7fb280ce3b45235bb54bc17
Girts Kesteris <girts.kesteris@gmail.com>
2024-01-27 13:44:11 +0200

    register poopsies

--- Game/src/poop.gd
+++ Game/src/poop.gd
@@ -3,7 +3,11 @@ class_name Poop
 
 var force := 25
 @onready var timer: Timer = get_node("Timer")
+@onready var area := get_node("Area3D")
 
+func _ready():
+	self.area.body_entered.connect(areaEntered)
+	
 func fire(dir: Vector3):
 	apply_impulse(dir * force)
 	
@@ -11,3 +15,7 @@ func cleanup(time: float):
 	timer.wait_time = time
 	timer.start()
 	timer.timeout.connect(self.queue_free)
+	
+func areaEntered(body):
+	if(body is Player):
+		queue_free()
