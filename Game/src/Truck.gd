5ee01f9cce17ed4fd738bae69defa487a38039e6
Girts Kesteris <girts.kesteris@gmail.com>
2024-01-28 16:11:45 +0200

    shitpile logic

--- Game/src/Truck.gd
+++ Game/src/Truck.gd
@@ -3,6 +3,7 @@ class_name Truck
 
 @onready var area: Area3D = $"Area3D"
 @onready var deliverSFX: = $"ShitDeliverSFX"
+@onready var shit:= $"PileOfShit/DeleteNow/PileOfShitForCar"
 
 func _ready():
 	Global.truck = self
@@ -15,5 +16,13 @@ func bodyEntered(body):
 		deliverSFX.pitch_scale = randf_range(0.5, 1.5)
 		deliverSFX.play()
 		
+		updateShapes()
+		
 		await get_tree().physics_frame
 		body.queue_free()
+		
+func updateShapes():
+	var shitProgress: float = inverse_lerp(0, 100, Global.gm.poopsiesRemoved)
+	
+	shit.set_blend_shape_value(0, lerp(-1, 1, shitProgress))
+	shit.set_blend_shape_value(1, lerp(1, -1, shitProgress))
