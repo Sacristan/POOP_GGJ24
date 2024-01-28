285615132df4b797718da4b021b3ab8c5a1fbea8
Girts Kesteris <girts.kesteris@gmail.com>
2024-01-28 13:39:40 +0200

    add truck pitch

--- Game/src/Truck.gd
+++ Game/src/Truck.gd
@@ -2,6 +2,7 @@ extends Node3D
 class_name Truck
 
 @onready var area: Area3D = $"Area3D"
+@onready var deliverSFX: = $"ShitDeliverSFX"
 
 func _ready():
 	Global.truck = self
@@ -10,6 +11,9 @@ func _ready():
 func bodyEntered(body):
 	if(body is Shit):
 		Global.poopsiesDelivered(body)
+		
+		deliverSFX.pitch_scale = randf_range(0.5, 1.5)
+		deliverSFX.play()
 		
 		await get_tree().physics_frame
 		body.queue_free()
