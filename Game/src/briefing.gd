ee07f537993afb65d2eeb11050ac238144b9a83b
Girts Kesteris <girts.kesteris@gmail.com>
2024-01-28 12:10:28 +0200

    add briefing

---
+++ Game/src/briefing.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var briefingLapsed = 0
+const TypeSpeed = 0.1
+
+@onready var briefingText: RichTextLabel = $briefingtext
+
+func _ready():
+	briefingLapsed = 0
+	briefingText.visible_characters = 0
+	
+	set_physics_process(false)
+	
+	await Global.wait(1)
+	set_physics_process(true)
+
+func _process(delta):
+	if(Input.is_action_just_pressed("ui_exit")):
+		Global.launchGame()
+
+func _physics_process(delta):
+	briefingLapsed += delta
+	briefingText.visible_characters = briefingLapsed/TypeSpeed
--- Game/src/menu.gd
+++ Game/src/menu.gd
@@ -1,9 +1,8 @@
 extends Node3D
 
-
 @onready var playButton: Button = $"MenuUI/Control/VBoxContainer/Play"
 @onready var quitButton: Button = $"MenuUI/Control/VBoxContainer/Quit"
 
 func _ready():
-	playButton.pressed.connect(Global.launchGame)
+	playButton.pressed.connect(Global.launchBriefing)
 	quitButton.pressed.connect(Global.quitGame)
